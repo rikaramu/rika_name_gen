9331fe73b369b549ec9fc58f7d5b0d12e717fc10
rikaramu <rikaramu@hotmail.com>
2021-03-10 10:39:47 +0700

    init

---
+++ test/rika_name_gen_test.dart
@@ -0,0 +1,16 @@
+import 'package:rika_name_gen/rika_name_gen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
